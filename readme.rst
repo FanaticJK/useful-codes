9b0851207678d98c1562829331c6d1e35854ff77
Sujan Karki <regolithsjk@gmail.com>
2016-11-07 17:48:53 +0545

    regex to chaeck validation of email;javascript

--- readme.rst
+++ readme.rst
@@ -3,3 +3,9 @@ File Get Content
 ###################
 
 This file contains a code that uses curl to get content of a remote file.
+
+###################
+Validate email using regex
+###################
+
+This file contains a code that uses regex to check if the email is filled and is a valid email.
---
+++ validate.php
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+	jQuery('.subscribe_button').click(function(){ 
+            var email = document.getElementById("price_to").value;
+            if(email == "" || !isValidEmailAddress(email)){
+                alert( 'Enter valid email.' );
+                return false;
+            }else{
+                return true;
+            }
+        });
+        function isValidEmailAddress(emailAddress) {
+            var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+            return pattern.test(emailAddress);
+        };
+</script>
